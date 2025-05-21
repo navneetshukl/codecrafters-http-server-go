72fd58817eba8490ab34ffb4ad6fbb04839c94ee
navneetshukl <navneetshukla824@gmail.com>
2025-05-21 22:50:05 +0530

    sending status OK

--- app/main.go
+++ app/main.go
@@ -22,9 +22,15 @@ func main() {
 		os.Exit(1)
 	}
 	
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	_,err=conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err!=nil{
+		fmt.Println("Error in sending the status OK: ",err.Error())
 		os.Exit(1)
 	}
 }
